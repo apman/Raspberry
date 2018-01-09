b3825b02b55a04d64a778cf224fe76acdc7ab360
apman <anna.malchowperryman@shopify.com>
2018-01-09 09:49:51 -0500

    more

--- C_Experiments/snake.c
+++ C_Experiments/snake.c
@@ -148,12 +148,12 @@ static int open_fbdev(const char *dev_name)
 void render()
 {
 	struct segment_t *seg_i;
-	memset(fb, 0x00F9, 128);
+	memset(fb, 0x00FF0000, 128);
 	//fb->pixel[apple.x][apple.y]=0xF800;
 	for(seg_i = snake.tail; seg_i->next; seg_i=seg_i->next) {
-		fb->pixel[seg_i->x][seg_i->y] = 0x7E0;
+		fb->pixel[seg_i->x][seg_i->y] = 0xFF000000;
 	}
-	fb->pixel[seg_i->x][seg_i->y]=0xF00F;
+	fb->pixel[seg_i->x][seg_i->y]=0x0000FF00;
 }
 
 int check_collision(int appleCheck)
