a415616f03c8c2206e8f8d1dcbd8879aebf471ef
apman <anna.malchowperryman@shopify.com>
2018-01-11 19:55:51 -0500

    mm

--- C_Experiments/snake.c
+++ C_Experiments/snake.c
@@ -161,7 +161,7 @@ void render()
 		fb->pixel[seg_i->x][seg_i->y] = 0x0FFF;
 	}
 	fb->pixel[seg_i->x][seg_i->y] = 0xFF0F;
-	fb->pixel[5->x][5->y] = 0xFFFF;
+	fb->pixel[5][5] = 0xFFFF;
 }
 
 int check_collision()
