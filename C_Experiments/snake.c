b0202617d41a76362115610b0adf26d071486170
apman <anna.malchowperryman@shopify.com>
2018-01-12 09:46:45 -0500

    green

--- C_Experiments/snake.c
+++ C_Experiments/snake.c
@@ -179,7 +179,7 @@ void addToPath() {
 
 void reset(void)
 {
-	//memset(fb, 0, 128); // turn all the lights off
+	memset(fb, 0x0F00, 128); // turn all the lights off
 
 	if (trailEnd != NULL) {
 		// return all allocated memory
